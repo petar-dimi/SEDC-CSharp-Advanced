6456236a62dc9be7c77840099075005efe408fd7
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-11-15 17:50:41 +0100

    non working days

---
+++ Classes/Class 01/Exercise 02/NonWorkingDays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NonWorkingDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<DateTime> nonWorkingDays = new List<DateTime> 
+            { 
+                new DateTime(2024, 1 ,1),
+                new DateTime(2024, 1, 7),
+                new DateTime(2024, 4, 20),
+                new DateTime(2024, 5, 1),
+                new DateTime(2024, 5, 25),
+                new DateTime(2024, 8, 3),
+                new DateTime(2024, 9, 8),
+                new DateTime(2024, 10, 12),
+                new DateTime(2024, 10, 23),
+                new DateTime(2024, 12, 8)
+
+
+            };
+
+            while (true)
+
+        }
+    }
+}
